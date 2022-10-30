51374ecf192cc93df0c9fb46a30476273c44cb73
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-10-30 11:16:36 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import { FormEvent, useState } from "react"
+import { AccountForm } from "./AccountForm"
+import { AddressForm } from "./AddressForm"
+import { useMultiStepForm } from "./useMultiStepForm"
+import { UserForm } from "./UserForm"
+
+type formData = {
+  firstName: string
+  lastName: string
+  age: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  email: string
+  password: string
+}
+
+const INITIAL_DATA: formData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: ""
+}
+
+function App() {
+  const [data, setData] = useState(INITIAL_DATA)
+  function updateFields(fields: Partial<formData>) {
+    setData(prev => {
+      return {...prev, ...fields}
+    })
+  }
+  const {steps, currentStepIndex, step, isFirstStep, isLastStep, next, back} = useMultiStepForm([
+    <UserForm {...data} updateFields={updateFields}/>, 
+    <AddressForm {...data} updateFields={updateFields}/>, 
+    <AccountForm {...data} updateFields={updateFields}/> 
+  ])
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault()
+    if(!isLastStep) return next()
+    alert("successful account creation")
+  }
+  return (
+    <div style={{
+      position:"relative",
+      background:"white",
+      border:"1px solid black",
+      padding:"2rem",
+      margin:"1rem",
+      borderRadius:".5rem",
+      fontFamily:"Arial",
+      maxWidth:"max-content"
+    }}>
+     <form onSubmit={onSubmit}>
+      <div style={{position:"absolute", top:"0.5rem", right:"0.5rem"}}>
+        {currentStepIndex + 1} / {steps.length}
+      </div>
+      {step}
+      <div style={{marginTop:"1rem", display:"flex", gap:"0.5rem", justifyContent:"flex-end"}}>
+        {!isFirstStep && <button type="button" onClick={back}>Back</button> }
+        <button type="submit">{isLastStep ? "Finish" : "Next" }</button>
+      </div>
+     </form>
+    </div>
+  )
+}
+
+export default App
